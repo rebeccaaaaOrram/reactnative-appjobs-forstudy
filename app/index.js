d8557ab0f29501922d5520645bb2992da8175427
rebeccaaaaOrram <rebecca.ignacio@orram.com.br>
2023-03-23 07:11:29 -0300

    chore: initial config

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import {
+    View,
+    Text
+} from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>olA</Text>
+        </View>
+    );
+}
+
+export default Home;
